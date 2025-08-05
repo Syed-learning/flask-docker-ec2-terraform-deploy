a9729d31be81915a4afcb3246437ef07a1678c1c
shereen19 <shaik.shereen1@gmail.com>
2025-08-05 11:54:19 +0530

    feedback.html commit

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Hello from Flask on Docker and AWS EC2!"
+
+@app.route('/feedback',methods=['GET','POST'])
+def feedback():
+    if request.method == 'POST':
+        name = request.form['name']
+        message = request.form['message']
+
+        with open('feedback.txt', 'a') as f:
+            f.write(f"Name: {name}\nMessage: {message}\n---\n")
+
+        return redirect('/feedback')
+    
+
+
+    return render_template('feedback.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
